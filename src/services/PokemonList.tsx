86ef986c081ed90adfbcf20867892f3513098c05
ProgramadorOrientadoaObjeto <eduardosbarros.ed@gmail.com>
2023-09-30 16:17:04 -0300

    Pokemons 0.1 FINAL

--- src/services/PokemonList.tsx
+++ src/services/PokemonList.tsx
@@ -1,68 +1,63 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react';
+import styles from './styles.module.css'
 
-const PokemonList = () => {
-  const [pokemonList, setPokemonList] = useState([]);
-  const [selectedPokemon, setSelectedPokemon] = useState(null);
-  const [nextPageUrl, setNextPageUrl] = useState("https://pokeapi.co/api/v2/pokemon");
-
-  const [pokemonDetails, setPokemonDetails] = useState(null);
-
-  useEffect(() => {
-    const fetchPokemonList = async () => {
-      try {
-        const response = await fetch(nextPageUrl);
-        const data = await response.json();
-        setPokemonList((prevList) => [...prevList, ...data.results]);
-        setNextPageUrl(data.next);
-      } catch (error) {
-        console.error("Erro ao buscar lista de Pokémon:", error);
-      }
-    };
-
-    fetchPokemonList();
-  }, [nextPageUrl]);
-
-  useEffect(() => {
-    const fetchPokemonDetails = async () => {
-      if (selectedPokemon) {
-        try {
-          const response = await fetch(selectedPokemon.url);
-          const data = await response.json();
-          setPokemonDetails(data);
-        } catch (error) {
-          console.error("Erro ao buscar detalhes do Pokémon:", error);
-        }
-      }
-    };
-
-    fetchPokemonDetails();
-  }, [selectedPokemon]);
-
-  const handlePokemonClick = (pokemon) => {
-    setSelectedPokemon(pokemon);
+type Pokemon = {
+  name: string;
+  sprites: {
+    front_default: string;
   };
+};
+
+function PokemonList() {
+  const [pokemonData, setPokemonData] = useState<Pokemon[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=151') // Obtém os primeiros 151 Pokémon
+      .then((response) => response.json())
+      .then((data) => {
+        const results = data.results;
+        const promises = results.map((result: { url: string }) =>
+          fetch(result.url).then((response) => response.json())
+        );
+
+        Promise.all(promises)
+          .then((data: Pokemon[]) => {
+            setPokemonData(data);
+            setIsLoading(false);
+          })
+          .catch((error) => console.error(error));
+      })
+      .catch((error) => console.error(error));
+  }, []);
+
+  if (isLoading) {
+    return <div>Carregando...</div>;
+  }
 
   return (
     <div>
-      <h1>Lista de Pokémon</h1>
-      <ul>
-        {pokemonList.map((pokemon, index) => (
-          <li key={index} onClick={() => handlePokemonClick(pokemon)}>
-            {pokemon.name}
-          </li>
+      <h1 className={styles.Title}>Pokémons:</h1>
+      <div className={styles.Pokemons}>
+        {pokemonData.map((pokemon, index) => (
+          <div key={index} className={styles.Pokemon}>
+            <img
+              src={pokemon.sprites.front_default}
+              alt={pokemon.name}
+              className={styles.PokemonSprite}
+            />
+            <div className={styles.PokemonInfo}>
+              <h2 className={styles.PokemonName}>{pokemon.name}</h2>
+              {/* Adicione mais informações aqui conforme necessário */}
+            </div>
+          </div>
         ))}
-      </ul>
-      {pokemonDetails && (
-        <div className="pokemon-detail">
-          <h2>{pokemonDetails.name}</h2>
-          <img src={pokemonDetails.sprites.front_default} alt={pokemonDetails.name} />
-          <p>ID: {pokemonDetails.id}</p>
-          <p>Tipo(s): {pokemonDetails.types.map((type) => type.type.name).join(", ")}</p>
-          {/* Adicione outras informações detalhadas do Pokémon conforme necessário */}
-        </div>
-      )}
+      </div>
+      <div className={styles.Final}>
+          <img className={styles.PokeAPIImg} src="pokeapi_256.3fa72200.png" alt="" />
+      </div>
     </div>
   );
-};
+}
 
 export default PokemonList;
